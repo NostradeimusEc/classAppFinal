78af663f003e5b3c7e27d931199412aee44b47dc
NostradeimusEc <davedavid.dq@gmail.com>
2023-11-15 03:03:42 -0300

    varios cambios

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { Page404Component } from './page404/page404.component';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -9,10 +7,7 @@ import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    Page404Component,
-  ],
+  declarations: [ ],
   imports: [
     CommonModule,
     IonicModule,
